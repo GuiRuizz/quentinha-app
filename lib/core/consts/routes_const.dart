4ff59d36bd049cea2fc0066fa05e089abb2029f2
Guilherme Sassi <dsrlikeaboss@gmail.com>
2025-09-15 08:35:18 -0300

    feat: Add a tela de registrar

---
+++ lib/core/consts/routes_const.dart
@@ -0,0 +1,6 @@
+class AppNameRoutes {
+  static const String home = '/';
+  static const String login = '/login';
+  static const String onboarding = '/onboarding';
+  static const String register = '/register';
+}
--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quentinha_app/core/consts/routes_const.dart';
+import 'package:quentinha_app/presentation/view/register_page.dart';
 
 import '../../presentation/view/home_page.dart';
 import '../../presentation/view/login_page.dart';
 import '../../presentation/view/onboarding_page.dart';
 
 class AppRoutes {
-  static const String home = '/';
-  static const String login = '/login';
-  static const String onboarding = '/onboarding';
+  
 
   static GoRouter router(bool seenOnboarding) {
     return GoRouter(
-      initialLocation: seenOnboarding ? home : onboarding,
+      initialLocation: seenOnboarding ? AppNameRoutes.home : AppNameRoutes.onboarding,
       routes: [
         GoRoute(
-          path: home,
+          path: AppNameRoutes.home,
           name: 'home',
           builder: (context, state) => const HomePage(),
         ),
         GoRoute(
-          path: login,
+          path: AppNameRoutes.login,
           name: 'login',
           builder: (context, state) => const LoginPage(),
         ),
         GoRoute(
-          path: onboarding,
+          path: AppNameRoutes.onboarding,
           name: 'onboarding',
           builder: (context, state) => const OnboardingPage(),
+        ),
+        GoRoute(
+          path: AppNameRoutes.register,
+          name: 'register',
+          builder: (context, state) => const RegisterPage(),
         ),
       ],
       errorBuilder: (context, state) => Scaffold(
