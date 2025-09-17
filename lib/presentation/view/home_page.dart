7cafd7ec2f15ee0b25a1b7c348c4c574c5f72885
Guilherme Sassi <dsrlikeaboss@gmail.com>
2025-09-17 16:05:12 -0300

    feat: Add o header, apresentação básica

--- lib/presentation/view/home_page.dart
+++ lib/presentation/view/home_page.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:quentinha_app/core/consts/colors_const.dart';
+import 'package:quentinha_app/core/consts/size_const.dart';
 
-class HomePage extends StatelessWidget {
+import '../../core/utils/date_time_utils.dart';
+import '../components/app_bar_widget.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("🏠 Home Page")),
+    final double topSafeArea = MediaQuery.of(context).padding.top;
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: topSafeArea),
+              AppBarPersonalizada(),
+              20.h,
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Olá {Nome do cliente}, ${DateUtilsHelper.saudacao()}!',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.start,
+                    ),
+                    20.h,
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Pesquisar',
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: AppColors.widgetBackgroundGrey,
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
